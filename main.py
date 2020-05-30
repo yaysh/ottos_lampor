1eb624418e61298aeb23f8ca52fba8f7d7eee66e
Jens Madsen <madswed@hotmail.com>
2020-05-30 14:07:06 +0200

    Added read ip address function

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
 
+def loadIPAddress():
+    f = open("ipaddress.txt", "r")
+    line = f.read()
+    return line.strip()
+
+
 def main():
-    IP_ADDRESS = loadIPAddress()
+    ip_address = loadIPAddress()
+    print(ip_address)
+    
 
 if __name__ == "__main__":
     main()
